c27e515008b13454bd6872598e44fb80730ce966
MonicaGonzalez30 <monik196901@gmail.com>
2023-12-04 14:15:14 -0600

    Creación del login

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './comun/header/header.component';
 import { FooterComponent } from './comun/footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
