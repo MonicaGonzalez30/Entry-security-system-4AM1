07fbb0cd46781fc92262494469afb5bc5518c1f7
MonicaGonzalez30 <monik196901@gmail.com>
2024-01-01 20:49:27 -0600

    Avance sección editar datos y corrección registro

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RegistroComponent } from './registro/registro.component';
 import { PreguntasComponent } from './paginas/preguntas/preguntas.component';
 import { CodigoComponent } from './paginas/codigo/codigo.component';
 import { VerDatosComponent } from './paginas/ver-datos/ver-datos.component';
+import { EditarDatosComponent } from './paginas/editar-datos/editar-datos.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
@@ -11,5 +12,6 @@ export const routes: Routes = [
     {path: 'preguntas', component: PreguntasComponent},
     {path: 'codigo', component: CodigoComponent},
     {path: 'verDatos', component: VerDatosComponent},
+    {path: 'editarDatos', component: EditarDatosComponent},
     {path: '', redirectTo: '/login', pathMatch: 'full'} //Ruta por defecto
 ];
