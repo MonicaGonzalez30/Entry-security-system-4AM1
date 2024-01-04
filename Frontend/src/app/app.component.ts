6ce9584502682384bcc6559603b1e4f1b95d5b1c
MonicaGonzalez30 <monik196901@gmail.com>
2024-01-04 15:41:48 -0600

    Avance del servicio de login

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContentComponent } from './content/content.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginService } from './servicios/login.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContentComponent],
+  imports: [CommonModule, RouterOutlet, ContentComponent, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [LoginService]
 })
 export class AppComponent {
-  title = 'Frontend';
+  title = 'ESS ESCOM';
 }
--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { LoginService } from '../servicios/login.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  public idUsuario!: string;
+  public password!: string;
 
+  constructor(private uService: LoginService, private ruteador: Router){ }
+
+  public Iniciar(){
+    this.uService.LoginUser(this.idUsuario, this.password).subscribe(resultado => {
+      //console.log(resultado);
+      this.ruteador.navigate(["preguntas"]);
+    });
+  }
 }
