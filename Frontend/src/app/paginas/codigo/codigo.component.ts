023465cf9ac419e1ff7c6de51610133f339b269d
MonicaGonzalez30 <monik196901@gmail.com>
2024-01-10 08:18:34 -0600

    Se agregó el QR del usuario

--- Frontend/src/app/paginas/codigo/codigo.component.ts
+++ Frontend/src/app/paginas/codigo/codigo.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../comun/header/header.component';
 import { FooterComponent } from '../../comun/footer/footer.component';
+import { UsuarioService } from '../../servicios/usuario.service';
 
 @Component({
   selector: 'app-codigo',
@@ -10,5 +11,13 @@ import { FooterComponent } from '../../comun/footer/footer.component';
   styleUrl: './codigo.component.scss'
 })
 export class CodigoComponent {
+  public idUsuario!: string;
+  public qrImagen!: any;
 
+  constructor(private usuarioService: UsuarioService) { }
+
+  ngOnInit() {
+    this.idUsuario = this.usuarioService.idUsuario;
+    this.qrImagen = "https://localhost:7276/QR/datos/"+this.idUsuario;
+  }
 }
